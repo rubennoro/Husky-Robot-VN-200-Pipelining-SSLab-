ef36dcd0655a1664c26e72c17c3195bbcb751721
rubennoro <160801172+rubennoro@users.noreply.github.com>
2024-08-01 11:50:04 -0400

    Add files via upload

---
+++ m4_process/m4_process.ino
@@ -0,0 +1,24 @@
+#include "vn200_arduino_serial.h"
+#include <RPC.h>
+// #include "share_memory_m4.h"
+
+void setup(){
+  RPC.begin();
+  USERIAL = &RPC;
+
+  Serial1.begin(921600);
+  //RPC.println("Reached m4");
+}
+
+void loop(){
+  // RPC.print("hello world");
+  //RPC.println("hello world");
+  read_vn200();
+  //RPC.print(a_x.f,4);
+  // RPC.print("ss ");
+  // delay(1000);
+
+}
+  
+
+
